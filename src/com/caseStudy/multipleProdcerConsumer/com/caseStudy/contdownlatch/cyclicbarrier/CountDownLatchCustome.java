92e83de63c117c44cb6d3a844c2eab142ae3a272
poonam <poonam_chate@rediffmail.com>
2018-04-11 14:43:07 +0530

    Case Study for count down latch and cyclic barrier custom implementation.

---
+++ src/com/caseStudy/multipleProdcerConsumer/com/caseStudy/contdownlatch/cyclicbarrier/CountDownLatchCustome.java
@@ -0,0 +1,24 @@
+package com.caseStudy.multipleProdcerConsumer.com.caseStudy.contdownlatch.cyclicbarrier;
+
+public class CountDownLatchCustome {
+
+    int count;
+
+    public CountDownLatchCustome(int count){
+        this.count = count;
+    }
+
+    public synchronized void await() throws InterruptedException{
+        if(count > 0){
+            this.wait();
+        }
+    }
+
+    public synchronized void countDown(){
+        count--;
+
+        if(count == 0){
+            this.notifyAll();
+        }
+    }
+}
---
+++ src/com/caseStudy/multipleProdcerConsumer/com/caseStudy/contdownlatch/cyclicbarrier/CyclicBarrierCustom.java
@@ -0,0 +1,35 @@
+package com.caseStudy.multipleProdcerConsumer.com.caseStudy.contdownlatch.cyclicbarrier;
+
+public class CyclicBarrierCustom {
+
+    int initialParties;
+    int partiesAwait;
+    Runnable barrierPoint;
+
+    public CyclicBarrierCustom(int parties, Runnable barrierPoint){
+        this.initialParties = parties;
+        this.partiesAwait = parties;
+        this.barrierPoint = barrierPoint;
+    }
+
+    public synchronized void await() throws InterruptedException{
+        partiesAwait--;
+
+        //If the current thread is not the last to arrive, thread will wait.
+        if(partiesAwait>0){
+            this.wait();
+        }
+           /*If the current thread is last to arrive, notify all waiting threads, and
+            launch event*/
+        else{
+                  /* All parties have arrive, make partiesAwait equal to initialParties,
+                    so that CyclicBarrier could become cyclic. */
+            partiesAwait=initialParties;
+
+            notifyAll(); //notify all waiting threads
+
+            barrierPoint.run(); //launch event
+        }
+
+    }
+}
