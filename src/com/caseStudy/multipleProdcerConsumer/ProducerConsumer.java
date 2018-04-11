4a20bf0d1a1f376f9b95b5215311388b950d850e
poonam <poonam_chate@rediffmail.com>
2018-04-11 09:53:41 +0530

    Usecase for creating 10 producers and 10 consumers.

--- src/com/caseStudy/multipleProdcerConsumer/ProducerConsumer.java
+++ src/com/caseStudy/multipleProdcerConsumer/ProducerConsumer.java
@@ -12,40 +12,40 @@ public class ProducerConsumer {
 
         final PC pc = new PC();
 
-        Thread producer = new Thread(()->{
-            try{
-                pc.producer();
-            }catch (InterruptedException e){
-                logger.log(Level.INFO,"Interrupted exception in producer.");
-            }
+        for(int i=1;i<=10;i++){
+            /*Thread producer =*/ new Thread(()->{
+                try{
+                    pc.producer(Thread.currentThread().getName());
+                }catch (InterruptedException e){
+                    logger.log(Level.INFO,"Interrupted exception in producer.");
+                }
+            }).start();
+            /*Thread consumer = */new Thread(()->{
+                try{
+                    pc.consumer(Thread.currentThread().getName());
+                }catch (InterruptedException e){
+                    logger.log(Level.INFO,"Interrupted exception in consumer.");
+                }
+            }).start();
+            //  producer.start();
+        }
 
-        });
-        Thread consumer = new Thread(()->{
-            try{
-                pc.consumer();
-            }catch (InterruptedException e){
-                logger.log(Level.INFO,"Interrupted exception in consumer.");
-            }
-        });
-        producer.start();
-        consumer.start();
-
-        producer.join();
-        consumer.join();
+        /*producer.join();
+        consumer.join();*/
     }
 
     public static class PC{
         private LinkedList<Integer> queue = new LinkedList<>();
         int value = 0;
         int capacity = 10;
-        public void producer() throws InterruptedException{
+        public void producer(String threadName) throws InterruptedException{
             while(true){
                 synchronized (this){
                     if(queue.size() == capacity){
                         wait();
                     }
                     queue.add(value++);
-                    System.out.println("Produced: "+ value);
+                    System.out.println("Produced: "+ value + " by Thread:"+ threadName);
                     notifyAll();
 
                     Thread.sleep(1000);
@@ -54,14 +54,14 @@ public class ProducerConsumer {
 
         }
 
-        public void consumer() throws InterruptedException{
+        public void consumer(String threadName) throws InterruptedException{
             while(true){
                 synchronized (this){
                     if(queue.size() == 0){
                         wait();
                     }
                     int consumed = queue.removeFirst();
-                    System.out.println("Consumed: "+consumed);
+                    System.out.println("Consumed: "+consumed + " by Thread:"+ threadName);
                     notifyAll();
                     Thread.sleep(1000);
                 }
